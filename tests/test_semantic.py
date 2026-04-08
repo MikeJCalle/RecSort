09760ce4eb6ca26fa09002dc7ad30dae3f383366
MikeJCalle <mjc7943@psu.edu>
2026-04-07 23:57:04 -0400

    Update test_semantic.py

--- tests/test_semantic.py
+++ tests/test_semantic.py
@@ -1,9 +1,9 @@
 """
-Tests for NestSort semantic analysis stage.
+Tests for RecSort semantic analysis stage.
 """
 
 import pytest
-from nestsort.compiler import parse, analyse, SemanticError
+from recsort.compiler import parse, analyse, SemanticError
 
 
 def test_valid_flat_passes():
